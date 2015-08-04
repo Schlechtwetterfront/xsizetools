7374ddad3c8f0678609a0f3a207e6cb785af13bd
Benedikt Schatz <schlechtwetterfront@gmail.com>
2015-08-04 02:49:06 +0200

    New design.

--- javascripts/ande.js
+++
@@ -1,6 +0,0 @@
-function toggleHide(divID) {
-    var item = document.getElementById(divID);
-    if (item) {
-        item.className = (item.className == 'hidden') ? 'unhidden' : 'hidden';
-    }
-}
